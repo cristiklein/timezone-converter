f1a23aadf11287818d85e6c0ccaad21836c2d139
Cristian Klein <cristian.klein@elastisys.com>
2025-11-12 13:48:12 +0100

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg"],
+      manifest: {
+        name: "Time Zone Converter",
+        short_name: "TimeZ",
+        description: "Quickly compare times across world time zones",
+        theme_color: "#111111",
+        background_color: "#000000",
+        display: "standalone",
+        start_url: "./",
+        icons: [{
+          "src": "favicon.svg",
+          "sizes": "512x512",
+          "type": "image/svg+xml"
+        }],
+      },
+    }),
+  ],
   base: "/timezone-converter/",
 })
